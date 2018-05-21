f592aeab87d53a265fe462b374d7ac127141aac8
Julius Alvarado <angularjs.uiux@gmail.com>
2018-05-20 20:37:37 -0700

    starting to code the graph struct

--- app_server/routes/graph.js
+++ app_server/routes/graph.js
@@ -2,42 +2,60 @@
  * Created by Julius Alvarado on 5/19/2018.
  */
 
+// The Dictionary Class
 function Dictionary() {
     let items = {};
 
-    this.set = function () {
-
+    this.set = function (key, value) {
+        items[key] = value;
     };
 
-    this.get = function () {
-
+    this.get = function (key) {
+        return this.has(key) ? items[key] : undefined;
     };
 
-    this.keys = function () {
-
+    this.keys = function (key) {
+        return key in items;
     };
 
-    this.remove = function(){
-
+    this.remove = function (key) {
+        if (this.has(key)) {
+            delete items[key];
+            return true;
+        }
+        return false;
     };
 
-    this.clear = function(){
-
+    this.clear = function () {
+        items = {};
     };
 
-    this.has = function(){
-
+    this.has = function (key) {
+        return key in items;
     };
 
-    this.values = function(){
-
+    this.values = function () {
+        let values = [];
+        for (let k in items) {
+            if (items.hasOwnProperty(k)) {
+                if (this.has(k)) {
+                    values.push(items[k]);
+                }
+            }
+        }
     };
 
-    this.size = function(){
+    this.size = function () {
+        return Object.keys(items).length;
+    };
 
+    this.getItems = function (key) {
+        return items;
     };
 }
 
+// The Graph Class
 function Graph() {
-
+    let verticies = [];
+    let adjList = new Dictionary();
 }
